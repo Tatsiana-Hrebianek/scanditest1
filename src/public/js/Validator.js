56513933c8021aa6d17af325ef27735963c72985
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2022-10-19 00:59:31 +0300

    JS-3

--- src/public/js/Validator.js
+++ src/public/js/Validator.js
@@ -1,10 +1,9 @@
-class Validation {
+class Validator {
     disk = document.getElementById("Disk");
     book = document.getElementById("Book");
     furniture = document.getElementById("Furniture");
 
     constructor() {
-        //initializing properties with our parameters
         this.sku = document.getElementById("sku");
         this.name = document.getElementById("name");
         this.price = document.getElementById("price");
@@ -14,7 +13,7 @@ class Validation {
         this.height = document.getElementById("height");
         this.width = document.getElementById("width");
         this.length = document.getElementById("length");
-        this.props = [this.sku, this.name, this.price, this.size, this.selector, this.length, this.width, this.height, this.weight];
+        this.properties = [this.sku, this.name, this.price, this.size, this.selector, this.length, this.width, this.height, this.weight];
 
         // let props = ["sku", "name", "price", "size", "type", "length", "width", "height", "weight"];
         // //let props = this.props;
@@ -25,10 +24,11 @@ class Validation {
         // })
     }
 
+
     checkValidity() {
-        let props = this.props;
-        props.forEach(function (elem) {
-            let error = document.querySelector(`#${elem.name} + span.error`);
+        let properties = this.props;
+        properties.forEach(function (elem) {
+            let error = document.querySelector(`#${elem.id} + span.error`);
             elem.addEventListener('input', function (e) {
                 if (elem.validity.valid) {
                     error.textContent = '';
@@ -95,11 +95,85 @@ class Validation {
 
     }
 
+
+    attr = [];
+    error = [];
+    errormessage = [];
+
+    //with handle method we will loop through all constraints and check which one failed and put it into
+    handle() {
+        let constraints = [this.isRequired(), this.isAlphaNumeric(), this.hasMax()];
+        console.log(constraints);
+
+        constraints.forEach(function (elem) {
+            elem.addEventListener('click keypress', function (e) {
+                if (elem.validity.valid) {
+                    return true;
+                    //this.errormessage.textContent = '';
+                } else {
+                    return false;
+                }
+
+                // else if (elem.validity.tooShort) {
+                //     error.textContent = `!!!The value should be at least ${elem.minLength}
+                //             characters; you enters ${elem.value.length}.`;
+                // }
+            });
+
+
+        });
+
+    }
+
+    isValid() {
+        if (this.handle()) {
+            console.log("the form is true");
+        } else {
+            console.log("the form is not valid");
+        }
+    }
+
+
+    isRequired(attribute, errormessage) {
+        this.attr = document.querySelector(`${attribute}`);
+        this.attr.setAttribute("required", "required");
+        this.error = document.querySelector(`${attribute}+span.error`);
+        this.error.setAttribute("required", "required");
+        this.errormessage = errormessage;
+        return this.attr;
+
+        //console.log(this.attr);
+    }
+
+    isAlphaNumeric(attribute, errormessage) {
+        this.attr = document.querySelector(`${attribute}`);
+        this.attr.setAttribute("required", "required pattern = '[A-Za-z\d]+'");
+        this.error = document.querySelector(`${attribute}+span.error`);
+        this.error.setAttribute("required", "required");
+        this.errormessage = errormessage;
+        return this.attr;
+        //console.log(this.error);
+    }
+
+    hasMax(attribute, errormessage) {
+        //this.attr = attribute;
+        this.attr = document.querySelector(`${attribute}`);
+        this.attr.setAttribute("hasMax", "maxlength = 10");
+        //let attr = this.attr;
+        this.error = document.querySelector(`${attribute}+span.error`);
+        this.error.setAttribute("required", "required");
+        this.errormessage = errormessage;
+        //this.error.textContent = errormessage;
+        return this.attr;
+        //console.log(this.error);
+    }
+
+
     checkOnSubmitEvent() {
-        let props = this.props;
+        let properties = this.properties;
         document.forms.product_form.addEventListener('submit', function (e) {
-            props.forEach(function (elem) {
-                let error = document.querySelector(`#${elem.name} + span.error`);
+            properties.forEach(function (elem) {
+                let error = document.querySelector(`#${elem.id} + span.error`);
                 if (!elem.validity.valid) {
                     e.preventDefault();
                     error.textContent = `Please, insert ${elem.name}`;
@@ -107,10 +181,25 @@ class Validation {
             });
         });
     }
+
 }
 
-let obj = new Validation();
-obj.checkValidity();
-obj.checkProductType();
-obj.checkOnSubmitEvent();
-obj.checkSKU();
+
+let validator = new Validator();
+validator.isRequired("#sku", "Please fill in the sku!");
+validator.isRequired("#name", "Please fill in the name!");
+
+// validator.isAlphaNumeric("#sku", "SKU must be alpha number");
+// validator.hasMax("#sku", "SKU must be maximum 10 characters");
+// validator.handle();
+// validator.isValid();
+
+// obj.checkValidity();
+// obj.checkProductType();
+// obj.checkOnSubmitEvent();
+// obj.checkSKU();
+
+
+
+
+
