ab7fa65c5342ef45f797543dbd71dbe446e558f1
Tatsiana-Hrebianek <108133519+Tatsiana-Hrebianek@users.noreply.github.com>
2022-11-25 13:44:27 +0300

    Update AddProductsController.php

--- src/app/controllers/AddProductsController.php
+++ src/app/controllers/AddProductsController.php
@@ -11,15 +11,16 @@ class AddProductsController extends Controller
 {
     public function action()
     {
+        $product;
 
         $productClass = $_POST['type'];
-        if ($productClass = 'Disc') {
+        if ($productClass === 'Disc') {
             $product = new Disc($_POST);
         }
-        if ($productClass = 'Book') {
+        if ($productClass === 'Book') {
             $product = new Book($_POST);
         }
-        if ($productClass = 'Furniture') {
+        if ($productClass === 'Furniture') {
             $product = new Furniture($_POST);
         }
         //Doesn't find class!!!
