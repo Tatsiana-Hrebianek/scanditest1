7cbbf68c6f063ab02a7410f40a2a1887561be40f
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2023-02-26 16:44:05 +0300

    Added action method to callback in Router, added GET and POST method to addRoute() method as parameter in Router class

--- src/App/Controllers/Router.php
+++ src/App/Controllers/Router.php
@@ -4,68 +4,68 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-use App\Controllers\CheckSKUController;
-use App\Controllers\ShowProductsController;
-
-
+/**
+ *
+ */
 class Router
 {
-    private array $handlers;
+    /**
+     * @var array
+     */
+    private array $routes;
+    /**
+     * @var
+     */
     private $notFoundHandler;
-    //let's create constants for POST and GET requests
-    private const METHOD_POST = 'POST';
-    private const METHOD_GET = 'GET';
-
 
     //path is a string and handler that is callable or a function or a php-class that is going to be executed once router matches the path,
     //that we are going to access
     //in order to add handler we might need to add an array of handlers
 
-    public function get(string $path, $handler): void
+    /**
+     * @param string $path
+     * @param $handler
+     * @param $method
+     * @return void
+     */
+    public function addRoute(string $path, $handler, $method)
     {
-        //and in order to add this path with handler in a handler array we are going to
-        //add a key and a path because the same path can match different request methods like GET, POST
-
-        $this->addHandler(self::METHOD_GET, $path, $handler);
-
-    }
-
-    public function post(string $path, $handler): void
-    {
-        $this->addHandler(self::METHOD_POST, $path, $handler);
-    }
-
-    //in order to avoid the repetetive code let's add an addHandler method
-
-    private function addHandler(string $method, string $path, $handler): void
-    {
-        $this->handlers[$method . $path] = [
+        $this->routes[$method . $path] = [
             'path' => $path,
             'method' => $method,
             'handler' => $handler,
         ];
     }
 
+    //???
+
+    /**
+     * @param $handler
+     * @return void
+     */
     public function addNotFoundHandler($handler): void
     {
         $this->notFoundHandler = $handler;
     }
 
 
+    /**
+     * @param $container
+     * @return void
+     */
     public function route($container)
     {
-
-        //let's get url from the server
         $requestUri = parse_url($_SERVER['REQUEST_URI']);
-        //this requestPath will be used in order to check the path of handler
         $requestPath = $requestUri['path'];
         $method = $_SERVER['REQUEST_METHOD'];
 
         $callback = null;
-        foreach ($this->handlers as $handler) {
-            if ($handler['path'] === $requestPath && $handler['method'] === $method) {
+        foreach ($this->routes as $route) {
+            if ($route['path'] === $requestPath && $route['method'] === $method) {
                 //if we find the correct path we can get a handler back
-                $callback = $handler['handler'];
+                $handler = $container->get($route['handler']);
+                $callback = [$handler, 'action'];
+
             }
         }
 
@@ -73,19 +73,16 @@ class Router
             $parts = explode('::', $callback);
             if (is_array($parts)) {
                 $className = array_shift($parts);
-
                 $className = explode('\\', $className);
                 $className = array_pop($className);
 
                 $handler = $container->get($className);
 
-                //array_pop
                 $method = array_shift($parts);
                 $callback = [$handler, $method];
 
             }
         }
-
 
         if (!$callback) {
             header("HTTP/1.0 404 Not Found");
@@ -94,13 +91,9 @@ class Router
             }
         }
 
-        //and we can execute this callback with function:
-//        return $callback;
-
         call_user_func_array($callback, [
             array_merge($_POST, $_GET),
         ]);
-
 
     }
 }
--- src/App/config/routes.php
+++ src/App/config/routes.php
@@ -4,12 +4,11 @@ declare(strict_types=1);
 use App\Controllers\Router;
 
 $router = new Router();
-$router->get('/', ShowProductsController::class . '::action');
-$router->post('/', AddProductsController::class . '::action');
-$router->post('/deleteProducts', DeleteProductsController::class . '::action');
-$router->get('/addProduct', ShowProductFormController::class . '::action');
-$router->post('/checkSKU', CheckSKUController::class . '::action');
-//    $routes->add('blog_show', '/blog/{slug}')
-//        ->controller([BlogController::class, 'show'])
-//        ->methods(['GET']);
+$router->addRoute('/', ShowProductsController::class, 'GET');
+$router->addRoute('/', ShowProductsController::class, 'GET');
+$router->addRoute('/', AddProductsController::class, 'POST');
+$router->addRoute('/deleteProducts', DeleteProductsController::class, 'POST');
+$router->addRoute('/addProduct', ShowProductFormController::class, 'GET');
+$router->addRoute('/checkSKU', CheckSKUController::class, 'POST');
+
 return $router;
