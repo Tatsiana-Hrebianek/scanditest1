e6f2da34f5602c0c1f4c3b7748b4570fad28eebb
Tatsiana Hrebianek <tatsiana.hrebianek@gmail.com>
2023-02-28 13:51:15 +0300

    Added ATTR_ERRMODE and ERRMODE_WARNING attributes to PDO dependency in container.php file, corrected ProductsModel class to use prpared staments with bind-parameters and placehoders with bind-values, changed CheckSKU controller to get dependency of ProductsModel through DI container

--- src/App/Controllers/CheckSKUController.php
+++ src/App/Controllers/CheckSKUController.php
@@ -8,26 +8,16 @@ use App\Models\ProductsModel;
 use App\Controllers\Router;
 use PDO;
 
-class CheckSKUController
+class CheckSKUController extends Controller
 {
-    private $productsModel;
-
-    public function __constructor(ProductsModel $productsModel)
-    {
-        $this->productsModel = $productsModel;
-    }
-
     public function action()
     {
         header('Content-Type: application/json');
+        $container = include __DIR__ . '/../container.php';
         $inp = $_POST['param'];
         $value = $_POST[$inp];
         $error = $_POST['error'];
-//??? is DI supported for XHR requests
-        $dbconnection = new PDO(DSN, NAME, PASSWORD);
-        $row = (new ProductsModel($dbconnection))->checkSKU($value);
-//        $row = $this->productsModel->checkSKU($value);
-
+        $row = $container->get('ProductsModel')->checkSKU($value);
         if ($value === $row["$inp"]) {
             $mes = ['error1' => "$error"];
         } else {
